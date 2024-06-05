7d927520f8017e0ef844f0f57bccf194b5be12bf
Lie Shiki <krugerjurgen337@gmail.com>
2024-06-05 16:30:20 +0900

    update

--- Resource/Shader/Object3d.PS.hlsl
+++ Resource/Shader/Object3d.PS.hlsl
@@ -19,53 +19,33 @@ Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
+static const float32_t PI = 3.14159265f;
+
+float gauss(float x, float y, float sigma)
+{
+    float exponent = -(x * x * y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    
+    return exp(exponent) * rcp(denominator);
+}
+
 PixelShaderOutput main(VertexShaderOutput input) {
 	PixelShaderOutput output;
-    output.color.rgb = float32_t3(0.0f, 0.0f, 0.0f);
-    output.color.a = 1.0f;
-	
-    uint32_t width, height;
-    gTexture.GetDimensions(width, height);
-    float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
-	
-	
-	//float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f),gMaterial.uvTransform);
-	//float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
-
-	//if (gMaterial.enableLighting != 0) {//lightingする場合
-	//	float Ndotl = dot(normalize(input.normal), -gDirectionalLight.direction);
-	//	float cos = pow(Ndotl * 0.5f + 0.5f, 2.0f);
-	//	output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-	//	output.color.a = gMaterial.color.a * textureColor.a;
-	//	if (textureColor.a == 0.0) {
-	//		discard;
-	//	}
-	//	if (textureColor.a <= 0.5) {
-	//		discard;
-	//	}
-	//	if (output.color.a == 0.0) {
-	//		discard;
-	//	}
-	//}
-	//else {
-	//	output.color = gMaterial.color * textureColor;
-	//}
-	
-    //output.color = gTexture.Sample(gSampler, input.texcoord);
-    //float32_t2 correct = input.texcoord * (1.0f - input.texcoord.yx);
-    //float vignette = correct.x * correct.y * 16.0f;
-    //vignette = saturate(pow(vignette, 0.8f));
-    //output.color.rgb *= vignette;
-	
+    
+    float32_t weight = 0.0f;
+    float32_t kernel3x3[3][3];
+    
     for (int32_t x = 0; x < 3; ++x)
     {
         for (int32_t y = 0; y < 3; ++y)
         {
-            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
-            float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            output.color.rgb += fetchColor * kKernel3x3[x][y];
+            kernel3x3[x][y] = gauss(kIndex3x3[x][y].x, kIndex3x3[x][y].y, 2.0f);
+            weight += kernel3x3[x][y];
         }
+
     }
+    
+    output.color.rgb *= rcp(weight);
 	
 	return output;
 }
